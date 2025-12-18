ef3c511c88f8d1fb7dbaf7bafb7a1c48300e66e7
taniksehrawat <taniksehrawat0@gmail.com>
2025-12-18 13:11:36 +0530

    Fix 400: remove role from RegisterRequest DTO

--- src/main/java/com/sunsettle/dto/RegisterRequest.java
+++ src/main/java/com/sunsettle/dto/RegisterRequest.java
@@ -1,14 +1,18 @@
 package com.sunsettle.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
+
     private String name;
     private String email;
     private String password;
-    private String role; // ADMIN or CLIENT
+
 }
