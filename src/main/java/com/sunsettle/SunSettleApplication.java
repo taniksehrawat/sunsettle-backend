87bddadea1a54029584e4479556421af262beb04
taniksehrawat <taniksehrawat0@gmail.com>
2025-12-18 18:55:02 +0530

    tarrif table creation

--- src/main/java/com/sunsettle/SunSettleApplication.java
+++ src/main/java/com/sunsettle/SunSettleApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SunSettleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SunSettleApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SunSettleApplication.class, args);
+    }
 }
